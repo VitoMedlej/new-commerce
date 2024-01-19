5e60e6d4303108caa624d24c608b870dc7f99fa1
Vito Medlej <vito.medlej@gmail.com>
2024-01-19 09:53:32 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,12 +43,20 @@ try {
       const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ cache: 'no-store' ,next:{revalidate:0}})
       let res = await req.json()
       // console.log('res: ', res);
-      const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/99610321-b6ca-48b7-a4e1-6103923a30da/basket/Images`,{cache:'no-store', next: { revalidate: 0 } })
+      // const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/99610321-b6ca-48b7-a4e1-6103923a30da/basket/Images`,{cache:'no-store', next: { revalidate: 0 } })
 
       // const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/11c7944e-bdec-4962-bcad-f24fe432bca6/basket/Images`,{ next: { revalidate: 400 } })
-      let resImages : any = reqImages && await  reqImages.json();
+      // let resImages : any = reqImages && await  reqImages.json();
       // let res = {data:null}
-        // let resImages = null;
+
+        let resImages = {
+          MainCarousel: [
+              {
+                  img: "https://ad.kapoorwatch.com/content/images/banners/RolexDesk1920x6553.webp",
+                  text: ""
+              }
+          ]
+      };
       return (
         <PreLoader resImages={resImages || null} data={res?.data}/>
        )
